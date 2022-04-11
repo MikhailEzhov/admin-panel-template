b7de765b5091718dd5e3001073f97a9d069b432b
MikhailEzhov <Hammer.perm@gmail.com>
2022-04-11 10:55:19 +0500

    feat: added middleware to check for a string

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import elements from '../reducers/elements';
 import filters from '../reducers/filters';
 
 
+
+// Собственный Middleware, проверяет dispatch, проверяет действие переданное в dispatch
+// dispatch записан как next
+// если строка, то модифицирует dispatch и возвращает действие в виде объекта
+// если не строка, то возвратит оригинальный dispatch с оригинальным действием
+const stringMiddleware = () => (next) => (action) => { 
+    if (typeof action === 'string') {                   
+        return next({                                 
+            type: action                     
+        })
+    }
+    return next(action)                                 
+};  
+
+
 const store = createStore(
-    combineReducers({elements, filters}), // объединение отдельных reducers
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    combineReducers({elements, filters}),                                            // объединение отдельных reducers
+    compose(                                                                         // второй аргумент(соединение-композиция функций)
+        applyMiddleware(stringMiddleware),                                               // последовательно запускает все Middlewares
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()     // всегда внизу - для расширения Redux DevTools
+    ) 
 );
 
 export default store;
